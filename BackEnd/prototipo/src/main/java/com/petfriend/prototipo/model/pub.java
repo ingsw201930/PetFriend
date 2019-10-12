8073df580e99501f58646611374c1bd2b7e3b4b0
Dorian <dorian.r.mv@gmail.com>
2019-10-12 10:11:51 -0500

    Cambio herencia, ahora Joined

--- BackEnd/prototipo/src/main/java/com/petfriend/prototipo/model/pub.java
+++ BackEnd/prototipo/src/main/java/com/petfriend/prototipo/model/pub.java
@@ -1,11 +1,11 @@
 package com.petfriend.prototipo.model;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("1")
+@PrimaryKeyJoinColumn(name = "id_publicacion")
 public class pub extends Publicacion {
 	@Column(name = "LUGAR")
 	String lugar;
