5507edfc7a2893dc7cfb098f0192c6699e2d6ae7
Karen Celis <k-celis@javeriana.edu.co>
2019-10-12 11:18:13 -0500

    Modelo y Servicios

---
+++ FrontEnd/PetFriendFront/src/app/modelo/animal-compañia.spec.ts
@@ -0,0 +1,7 @@
+import { AnimalCompañia } from './animal-compañia';
+
+describe('AnimalCompañia', () => {
+  it('should create an instance', () => {
+    expect(new AnimalCompañia()).toBeTruthy();
+  });
+});
