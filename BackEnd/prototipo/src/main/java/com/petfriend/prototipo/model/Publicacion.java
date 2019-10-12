68630152d1743f69bcdf667e7a982aaad2de0e18
Dorian <dorian.r.mv@gmail.com>
2019-10-12 00:05:31 -0500

    Cambio herencia a Single Table

--- BackEnd/prototipo/src/main/java/com/petfriend/prototipo/model/Publicacion.java
+++ BackEnd/prototipo/src/main/java/com/petfriend/prototipo/model/Publicacion.java
@@ -6,21 +6,28 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
 
-@MappedSuperclass
-//@Entity
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="tipo_publicacion", 
+  discriminatorType = DiscriminatorType.INTEGER)
 public class Publicacion {
     @Id
     @GeneratedValue
-    //@Column(name = "ID")
+    @Column(name = "ID")
     private int id;
-    //@Column(name = "FECHA")
+    @Column(name = "FECHA")
     private Date fecha;//TODO
     @ManyToOne
+    @JoinColumn(name="ID_DUENIO", nullable=false)
     private Usuario duenho;
-    //@Column(name = "DESCRIPCION")
+    @Column(name = "DESCRIPCION")
     private String descripcion; //TODO
     //TODO --- foto
 
--- BackEnd/prototipo/src/main/java/com/petfriend/prototipo/model/pub.java
+++ BackEnd/prototipo/src/main/java/com/petfriend/prototipo/model/pub.java
@@ -1,9 +1,11 @@
 package com.petfriend.prototipo.model;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("1")
 public class pub extends Publicacion {
 	@Column(name = "LUGAR")
 	String lugar;
