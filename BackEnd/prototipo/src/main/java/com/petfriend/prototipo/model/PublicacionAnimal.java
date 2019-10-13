9bfa342fe96b1b0ee51c153d885c978634d2b0ed
Dorian <dorian.r.mv@gmail.com>
2019-10-13 10:58:40 -0500

    Eliminados inheritance redundantes

--- BackEnd/prototipo/src/main/java/com/petfriend/prototipo/model/PublicacionAnimal.java
+++ BackEnd/prototipo/src/main/java/com/petfriend/prototipo/model/PublicacionAnimal.java
@@ -3,13 +3,10 @@ package com.petfriend.prototipo.model;
 //import javax.persistence.Column;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 
 @Entity
 @PrimaryKeyJoinColumn(name = "id_publicacion")
-@Inheritance(strategy = InheritanceType.JOINED)
 public class PublicacionAnimal extends Publicacion {
 	@OneToOne(mappedBy = "publicacion")
 	private Animal animal;
