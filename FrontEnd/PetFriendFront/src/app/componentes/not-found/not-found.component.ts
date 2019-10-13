2b7c47a166956abacc7fcce66bb06d42961c6083
Karen Celis <k-celis@javeriana.edu.co>
2019-10-12 23:29:30 -0500

    NotFound

--- FrontEnd/PetFriendFront/src/app/componentes/not-found/not-found.component.ts
+++ FrontEnd/PetFriendFront/src/app/componentes/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NotFoundComponent implements OnInit {
 
