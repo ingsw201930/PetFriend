57a2d8c992b8d4d4b25bdd5aeda8612ddfba507d
Dorian <dorian.r.mv@gmail.com>
2019-10-15 00:11:16 -0500

    Test imagen en base de datos

--- BackEnd/prototipo/src/main/java/com/petfriend/prototipo/controller/UsuarioController.java
+++ BackEnd/prototipo/src/main/java/com/petfriend/prototipo/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.petfriend.prototipo.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import com.petfriend.prototipo.repositories.IPublicacionAdopcionRepositorio;
 import com.petfriend.prototipo.repositories.IUsuarioRepositorio;
 import com.petfriend.prototipo.model.PublicacionAdopcion;
 import com.petfriend.prototipo.model.Usuario;
+import com.petfriend.prototipo.model.Utils;
 
 @Controller
 public class UsuarioController {
@@ -17,12 +21,18 @@ public class UsuarioController {
 	private IUsuarioRepositorio usuarioRepo;
 	@Autowired
 	private IPublicacionAdopcionRepositorio<PublicacionAdopcion> adopRepo;
+
 	@GetMapping("/test")
 	public void test() {
-		Usuario u= new Usuario();
-		u.setContrasenha("123");
-		u.setCorreo("haha");
+		Usuario u = new Usuario("correo@gmail.com", "larecontra", new File("prototipo/pasaporte.jpg"));
 		usuarioRepo.save(u);
+
+		Usuario test = usuarioRepo.findById(1).orElse(null);
+		try {
+			Utils.byteToImage(test.getFoto(), new File("almacenada.jpg"));
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 		 //return "{\"message\": \"admin OK\"}";
 	 }
 	@GetMapping("/pubAnimal")
