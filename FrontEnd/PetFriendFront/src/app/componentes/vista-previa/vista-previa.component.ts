a08a04690355b84cb2463e56b7a6053ad071297b
Camilo Muñoz <ancamu99@hotmail.com>
2019-10-20 11:19:25 -0500

    vista previa

--- FrontEnd/PetFriendFront/src/app/componentes/vista-previa/vista-previa.component.ts
+++ FrontEnd/PetFriendFront/src/app/componentes/vista-previa/vista-previa.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vista-previa.component.css']
 })
 export class VistaPreviaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+    title = 'vista';
+    nombreAnimal = "Nombre";
+    especieAnimal = "Especie";
+    edadAnimal = "Edad";
+    generoAnimal = "Genero";
+    razaAnimal = "Raza";
+    color1Animal = "Color1";
+    color2Animal = "Color2";
+    esterilizadoAnimal = "Si";
+    localidadAnimal = "LocalidadAnimal";
+    descripcionPublicacion = "Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción "
 
 }
