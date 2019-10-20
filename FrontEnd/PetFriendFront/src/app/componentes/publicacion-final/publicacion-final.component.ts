f6db0448e017251c0e63720ce1030bb373d2b824
Camilo Muñoz <ancamu99@hotmail.com>
2019-10-19 20:36:43 -0500

    Publicación final

--- FrontEnd/PetFriendFront/src/app/componentes/publicacion-final/publicacion-final.component.ts
+++ FrontEnd/PetFriendFront/src/app/componentes/publicacion-final/publicacion-final.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publicacion-final.component.css']
 })
 export class PublicacionFinalComponent implements OnInit {
+    title = 'PetFriend';
 
-  constructor() { }
-
-  ngOnInit() {
+    nombreAnimal = "Nombre";
+    especieAnimal = "Especie";
+    edadAnimal = "Edad";
+    generoAnimal = "Genero";
+    razaAnimal = "Raza";
+    color1Animal = "Color1";
+    color2Animal = "Color2";
+    esterilizadoAnimal = "Si";
+    localidadAnimal = "LocalidadAnimal";
+    descripcionPublicacion = "Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción "
   }
-
-}
