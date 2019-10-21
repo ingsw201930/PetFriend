7e925a3a6bd7d614ebc532bd99fb72bda57fe7b8
Karen Celis <k-celis@javeriana.edu.co>
2019-10-20 23:19:43 -0500

    Publicacion Final y Vista Previa

--- FrontEnd/PetFriendFront/src/app/componentes/publicacion-final/publicacion-final.component.ts
+++ FrontEnd/PetFriendFront/src/app/componentes/publicacion-final/publicacion-final.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publicacion-final.component.css']
 })
 export class PublicacionFinalComponent implements OnInit {
-
-  constructor() { }
-
+  title = 'PetFriend';
+  nombreAnimal = 'Nombre';
+  especieAnimal = 'Especie';
+  edadAnimal = 'Edad';
+  generoAnimal = 'Genero';
+  razaAnimal = 'Raza';
+  color1Animal = 'Color1';
+  color2Animal = 'Color2';
+  esterilizadoAnimal = 'Si';
+  localidadAnimal = 'LocalidadAnimal';
+  // tslint:disable-next-line: max-line-length
+  descripcionPublicacion = 'Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción Descripción ';
   ngOnInit() {
   }
-
 }
