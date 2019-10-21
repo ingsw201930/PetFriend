9c678a8c1836f6aad3345571a2664bc88caafa08
unknown <brayangarcia44@hotmail.com>
2019-10-20 22:11:31 -0500

    login

--- FrontEnd/PetFriendFront/src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ FrontEnd/PetFriendFront/src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class IniciarSesionComponent implements OnInit {
 
   constructor() { }
-
+  private titulo: string = 'Petfriend';
   ngOnInit() {
   }
 
