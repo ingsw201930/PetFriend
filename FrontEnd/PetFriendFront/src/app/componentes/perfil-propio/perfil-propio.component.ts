80f1860354df730c0ba99d32a3627d1e560309aa
Andres Camilo Muñoz <ancamu99@hotmail.com>
2019-11-19 16:23:53 -0500

    Pantalla perfil propio terminada

--- FrontEnd/PetFriendFront/src/app/componentes/perfil-propio/perfil-propio.component.ts
+++ FrontEnd/PetFriendFront/src/app/componentes/perfil-propio/perfil-propio.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-perfil-propio',
   templateUrl: './perfil-propio.component.html',
   styleUrls: ['./perfil-propio.component.css']
 })
 export class PerfilPropioComponent implements OnInit {
+  nombreUsuario: string;
+  correoUsuario: string;
+  direccionUsuario: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.nombreUsuario = 'aaa';
+    this.correoUsuario = 'aaa';
+    this.direccionUsuario = 'aaa';
   }
 
 }
