a6b9c9d3bbaf3bdb18de60232a9296cbfa644215
Karen Celis <k-celis@javeriana.edu.co>
2019-11-21 18:57:46 -0500

    RegistroEntidad

--- FrontEnd/PetFriendFront/src/app/componentes/registrar-usuario/registrar-usuario.component.ts
+++ FrontEnd/PetFriendFront/src/app/componentes/registrar-usuario/registrar-usuario.component.ts
@@ -14,10 +14,37 @@ export class RegistrarUsuarioComponent implements OnInit {
   direccion: string = '';
   telefono: string = '';
   usuario: Usuario = new Usuario;
+  imgURL: any;
+  public message: string;
 
+  imageSrc: string;
+
+  url1: any = 'assets/user.png';
   constructor() { }
 
   ngOnInit() {
+   
+  }
+
+  preview(files) {
+    const mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      console.log('Only images are supported.');
+      return;
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    // tslint:disable-next-line: variable-name
+    reader.onload = (_event) => {
+     
+       // this.service.urls[0]=reader.result;
+        //this.url1 = this.service.urls[0];
+        //console.log(this.url1);
+        this.url1 = reader.result;
+        console.log(this.url1);
+        return;
+      
+    };
   }
 registrarUsuario(){
 this.usuario.nombre=this.nombre;
