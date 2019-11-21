dea45ae464db65e324d5dc0c42adb5f300faff89
Karen Celis <k-celis@javeriana.edu.co>
2019-11-21 12:15:23 -0500

    Rutas

--- FrontEnd/PetFriendFront/src/app/app-routing.module.ts
+++ FrontEnd/PetFriendFront/src/app/app-routing.module.ts
@@ -7,8 +7,13 @@ import { PublicacionFinalComponent } from './componentes/publicacion-final/publi
 import { TiposDePublicacionComponent } from './componentes/tipos-de-publicacion/tipos-de-publicacion.component';
 import { VistaPreviaComponent } from './componentes/vista-previa/vista-previa.component';
 import { NotFoundComponent } from './componentes/not-found/not-found.component';
-
-
+import {AnimalesComponent} from './componentes/animales/animales.component';
+import {AnimalesSimilaresComponent} from './componentes/animales-similares/animales-similares.component';
+import {BusquedaComponent} from './componentes/busqueda/busqueda.component';
+import {RegistrarEntidadComponent} from './componentes/registrar-entidad/registrar-entidad.component';
+import {RegistrarUsuarioComponent} from './componentes/registrar-usuario/registrar-usuario.component';
+import {RegistroComponent} from './componentes/registro/registro.component';
+import {ResultadosBusquedaComponent} from './componentes/resultados-busqueda/resultados-busqueda.component';
 const routes: Routes = [
   { path: 'CrearUnaPublicacion', component: CrearPubicacionComponent },
   { path: 'IniciarSesion', component: IniciarSesionComponent },
@@ -17,6 +22,14 @@ const routes: Routes = [
   { path: 'TiposDePublicacion', component: TiposDePublicacionComponent },
   { path: 'VistaPrevia', component: VistaPreviaComponent },
   { path: '**', component: NotFoundComponent },
+  { path: 'Animales', component: AnimalesComponent },
+  { path: 'AnimalesSimilares', component: AnimalesSimilaresComponent },
+  { path: 'Busqueda', component: BusquedaComponent },
+  { path: 'RegistrarEntidad', component: RegistrarEntidadComponent },
+  { path: 'RegistrarUsuario', component: RegistrarUsuarioComponent },
+  { path: 'Registro', component: RegistroComponent },
+  { path: 'ResultadosBusqueda', component: ResultadosBusquedaComponent }
+
 ];
 
 @NgModule({
